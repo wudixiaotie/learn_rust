f1773128c5c7dc99df1fa53610b3ecd01ce4fd92
xiaotie <wudixiaotie@gmail.com>
2015-07-08 23:57:47 +0800

    update

--- syntax_and_semantics/src/main.rs
+++ syntax_and_semantics/src/main.rs
@@ -1,4 +1,8 @@
+// 版本 43.0.2357.130 (64-bit)
 fn main() {
-    let a: bool = true;
-    println!("Hello, world!{}", a);
+    let v = vec![1, 2, 3];
+
+    let v2 = v;
+
+    println!("v[0] is: {}", v[0]);
 }
