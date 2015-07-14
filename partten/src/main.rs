3a0f840ed481a399c2bc699cfdbca524c0db27dd
xiaotie <wudixiaotie@gmail.com>
2015-07-14 21:04:58 +0800

    update

--- partten/src/main.rs
+++ partten/src/main.rs
@@ -13,18 +13,38 @@ fn main() {
 
 
     let name = "Steve".to_string();
-    let mut x2 = Some(x);
+    let x2 = Some(x);
     match x2 {
         ref a @ Some(_) => println!("{:?}", a),
         _ => {}
     }
-    let mut x1: Option<Person> = Some(Person { name: Some(name) });
+    let x1: Option<Person> = Some(Person { name: Some(name) });
     match x1 {
-        Some(Person { name: ref a @ Some(_), .. }) => println!("{:?}", a),
+        Some(Person { name: ref a @ Some(_), .. }) => println!("a addr ={:p}", &a),
         _ => {}
+    }
+    match x1 {
+        Some(Person { name: a @ Some(_), .. }) => println!("ref a addr ={:p}", &a),
+        _ => {}
+    }
+
+
+
+
+    let x3 = OptionalInt::Value(5);
+
+    match x3 {
+        OptionalInt::Value(i) if i > 4 => println!("Got an int bigger than 4!"),
+        OptionalInt::Value(_) => println!("Got an int!"),
+        OptionalInt::Missing => println!("No such luck."),
     }
 }
 #[derive(Debug)]
 struct Person {
     name: Option<String>,
+}
+
+enum OptionalInt {
+    Value(i32),
+    Missing,
 }
