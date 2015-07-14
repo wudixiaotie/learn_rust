3a097b776c7fac096c29f8c7feed441c2983763b
xiaotie <wudixiaotie@gmail.com>
2015-07-14 16:36:59 +0800

    update

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,7 +1,19 @@
 struct SquareMeters(i64);
+struct Room {
+    square: SquareMeters,
+}
 fn main() {
     let m = (60,);
     let (a,) = m;
     let SquareMeters(room_size) = SquareMeters(30);
     println!("{}, {}", room_size, a);
+
+    let room1 = Room { square: SquareMeters(133) };
+    let SquareMeters(x) = room1.square;
+    println!("My room1's square is {} square meters.", x);
+    let b = "bbbb";
+    let c = (b,);
+    let (d,) = c;
+    println!("b:{} b addr:{:p} c:{} c addr:{:p}", b, &b, c.0, &c);
+    println!("b:{} b addr:{:p} c:{} c.0 addr:{:p} d:{} d addr:{:p}", b, &b, c.0, &(c.0), d, &d);
 }
