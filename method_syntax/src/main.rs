0688a3bf3bee7f98f00971da059d58b2e5ee6e0f
xiaotie <wudixiaotie@gmail.com>
2015-07-17 17:36:01 +0800

    update

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -11,6 +11,18 @@ fn main() {
 
     let d = c1.grow(2.0).area();
     println!("{}", d);
+
+    println!("{}", Circle::not_self());
+
+    let c4 = CircleBuilder::new()
+                .x(1.0)
+                .y(2.0)
+                .radius(3.6)
+                .finalize();
+
+    println!("area is {}", c4.area());
+    println!("x: {}", c4.x);
+    println!("y: {}", c4.y);
 }
 struct Circle {
     x: f64,
@@ -19,6 +31,10 @@ struct Circle {
 }
 
 impl Circle {
+    fn not_self() -> i32 {
+        32
+    }
+
     fn reference(&self) {
        println!("taking self by reference!");
     }
@@ -37,5 +53,36 @@ impl Circle {
 
     fn grow(&self, increment: f64) -> Circle {
         Circle { x: self.x, y: self.y, radius: self.radius + increment }
+    }
+}
+
+struct CircleBuilder {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+impl CircleBuilder {
+    fn new() -> CircleBuilder {
+        CircleBuilder { x: 0.0, y: 0.0, radius: 1.0, }
+    }
+
+    fn x(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.x = coordinate;
+        self
+    }
+
+    fn y(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.y = coordinate;
+        self
+    }
+
+    fn radius(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.radius = coordinate;
+        self
+    }
+
+    fn finalize(&self) -> Circle {
+        Circle { x: self.x, y: self.y, radius: self.radius }
     }
 }
