b66b816ee199a8a6d2267a435df6c250ab486325
xiaotie <wudixiaotie@gmail.com>
2015-07-19 00:50:35 +0800

    update

--- pointer/src/main.rs
+++ pointer/src/main.rs
@@ -4,10 +4,26 @@ fn main() {
     let p = &mut a;
     println!("p's addr:{:p}", &p);
     println!("out test_pointer p's addr:{:p}", &test_pointer(p));
+
+    let b = 1;
+    let bp = &b;
+    println!("in main bp's addr:{:p}", p);
+    pointer_addr(bp);
+    pointer_addr1(bp);
 }
 
 fn test_pointer(p: &mut i32) -> &mut i32 {
     *p = 32;
     println!("in test_pointer p's addr:{:p}", &p);
+    p
+}
+
+fn pointer_addr(p: &i32) -> &i32 {
+    println!("in pointer_addr p's addr:{:p}", p);
+    p
+}
+
+fn pointer_addr1(p: &i32) -> &i32 {
+    println!("in pointer_addr1 p's addr:{:p}", p);
     p
 }
--- string/src/main.rs
+++ string/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let s = "Hello, world!"; //string &'static str
+    let mut s1 = s.to_string();//String
+    s1.push_str("asdf");
+
+    println!("S:{}, S1:{}", s, s1);
+
+    let s2 = &s;//&str
+    println!("s2:{}", s2);
+
+    let s3 = s2.to_string();
+
+    println!("Hello, world! addr :{:p}", &"Hello, world!");
+    println!("s addr :{:p}", s);
+    println!("s1 addr :{:p}", &s1);
+    println!("s2 addr :{:p}", &s2);
+    println!("s3 addr :{:p}", &s3);
 }
