e284bdb2ff04381a5d02803a596d59fa36738902
xiaotie <wudixiaotie@gmail.com>
2015-07-19 12:12:16 +0800

    update

--- string/src/main.rs
+++ string/src/main.rs
@@ -10,9 +10,21 @@ fn main() {
 
     let s3 = s2.to_string();
 
-    println!("Hello, world! addr :{:p}", &"Hello, world!");
-    println!("s addr :{:p}", s);
+    println!("s addr :{:p}", &s);
     println!("s1 addr :{:p}", &s1);
     println!("s2 addr :{:p}", &s2);
     println!("s3 addr :{:p}", &s3);
+
+    println!("Hello, world! in main addr :{:p}", &"Hello, world!");
+    println!("Hello, world! in main addr :{:p}", &"Hello, world!");
+    ts();
+    let s4 = "Hello, world!".to_string(); //string &'static str
+    let s5 = "sb";
+    let s6 = s4 + s5;
+    println!("s6 addr:{}", s6);
+}
+
+
+fn ts() {
+    println!("Hello, world! in ts addr :{:p}", &"Hello, world!");
 }
