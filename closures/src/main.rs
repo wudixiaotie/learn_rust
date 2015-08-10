b54a3505da1e7d4b81dbf84775b2d26532a0af95
xiaotie <wudixiaotie@gmail.com>
2015-08-10 15:25:11 +0800

    update

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -4,4 +4,13 @@ fn main() {
     };
 
     assert!(2 == result(1));
+
+    let mut num = 5;
+
+    {
+        let mut add_num = move |x: i32| num += x;
+
+        add_num(5);
+    }
+    assert_eq!(5, num);
 }
